0551496e67c6db8fdb93a89e1cad966a9ef91d78
J Andrew McCormick <jandrewmc@gmail.com>
2016-02-04 12:34:13 -0500

    added code for transmitting to and receiving from arduino

--- main.cpp
+++ main.cpp
@@ -1,18 +1,3 @@
- 
- 
- /** RF24Mesh_Example_Master.ino by TMRh20
-  * 
-  * Note: This sketch only functions on -Arduino Due-
-  *
-  * This example sketch shows how to manually configure a node via RF24Mesh as a master node, which
-  * will receive all data from sensor nodes.
-  *
-  * The nodes can change physical or logical position in the network, and reconnect through different
-  * routing nodes as required. The master node manages the address assignments for the individual nodes
-  * in a manner similar to DHCP.
-  *
-  */
-  
 #include "RF24Mesh/RF24Mesh.h"  
 #include <RF24/RF24.h>
 #include <RF24Network/RF24Network.h>
@@ -21,10 +6,58 @@ RF24 radio(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ);
 RF24Network network(radio);
 RF24Mesh mesh(radio,network);
 
+typedef struct
+{
+	int16_t nodeid;
+	int16_t currentTemperature;
+	int16_t currentFanSpeed;
+	int16_t currentServoPosition;
+	float currentPressure;
+}incomingData;
+
+typedef struct
+{
+	int16_t nodeid;
+	int16_t fanSpeed;
+	int16_t servoPosition;
+}outgoingData;
+
+typedef struct
+{
+	unsigned long ms;
+	unsigned long counter;
+}payload_t;
+
+void getIncomingData()
+{
+	while(network.available())
+	{
+		RF24NetworkHeader header;
+		network.peek(header);
+
+		incomingData in;
+		
+		switch (header.type)
+		{
+			case 'M': 
+				network.read(neader, &in, sizeof(in));
+				break;
+			default:
+				payload_t payload;
+				network.read(header, &payload, sizeof(payload));
+				break;
+		}
+	}
+}
+
+void sendOutgoingData()
+{
+	
+}
+
 int main(int argc, char** argv) 
 {
 	mesh.setNodeID(0);
-
  	mesh.begin();
 
 	while(1)
@@ -32,21 +65,8 @@ int main(int argc, char** argv)
   		mesh.update();
 		mesh.DHCP();
   
-		while(network.available())
-		{
-			RF24NetworkHeader header;
-			network.peek(header);
-    
-			//need to receive an array containing the data that we need
-			uint32_t dat=0;
-			network.read(header,&dat,sizeof(dat)); 
-			printf("Rcv %u from 0%o\n",dat,header.from_node);
-
-			//calculate what needs to be sent to this node
-
-			//send to this node
-  		}
-		delay(2);
+		getIncomingData();
+		sendOutgoingData();
   	}
 	return 0;
 }
