b94c433933ee0fe10946b4966909564a0a3af0cd
J Andrew McCormick <jandrewmc@gmail.com>
2016-04-26 15:50:18 -0400

    final code

--- RegisterCode/RegisterCode.ino
+++ RegisterCode/RegisterCode.ino
@@ -2,39 +2,46 @@
 #include "RF24Network.h"
 #include "RF24Mesh.h"
 #include "DHT.h"
-#include <SFE_BMP180.h>
-#include <Wire.h>
 #include <PWMServo.h>
 
-RF24 radio(8, 10);
+//note: pins 9, 11, 12, 13 are used by the RF Communicator
+int radioCEPin = 10;
+int radioCSNPin = 8;
+int tempSensorPin = 5;
+int fanControlPin = 6;
+int motionSensorPin = 7;
+int pressureSensorPin = A4;
+int servoPin = 9;
+int fanPowerPin = 3;
+
+//mux pins A, B, and C are all outputs from the micro
+//mux output pin is an input to the micro
+int muxAPin = 1;
+int muxBPin = 2;
+int muxCPin = 3;
+int muxOutputPin = 4;
+
+RF24 radio(radioCEPin, radioCSNPin);
 RF24Network network(radio);
 RF24Mesh mesh(radio, network);
 
 typedef struct
 {
-	int16_t nodeid;
-	int16_t currentTemperature;
-	int16_t currentFanSpeed;
-	int16_t currentServoPosition;
-	float currentPressure;
+	uint16_t nodeid;
+	uint16_t currentTemperature;
+	uint16_t currentFanSpeed;
+	uint16_t currentServoPosition;
+	uint16_t currentPressure;
+	uint16_t isMotionDetected;
 } outgoingData;
 
 typedef struct
 {
-	int16_t nodeid;
-	int16_t fanSpeed;
-	int16_t servoPosition;
+	uint16_t fanSpeed;
+	uint16_t servoPosition;
 } incomingData;
 
-struct payload_t
-{
-	unsigned long ms;
-	unsigned long counter;
-};
-
-DHT tempSensor(7, DHT11);
-
-SFE_BMP180 pressure;
+DHT tempSensor(tempSensorPin, DHT11);
 
 PWMServo myservo;
 
@@ -44,92 +51,107 @@ outgoingData out;
 //for the register, based on DIP switches
 //on the device itself.  The input will be
 //in binary format.
-int getNodeID()
+int updateNodeID()
 {
-	int nodeID;
+	int nodeID = 0;
 
-	pinMode(4, OUTPUT);
-	pinMode(5, OUTPUT);
-	pinMode(6, OUTPUT);
-	pinMode(3, INPUT);
-	int state = 0;
+	pinMode(muxAPin, OUTPUT);
+	pinMode(muxBPin, OUTPUT);
+	pinMode(muxCPin, OUTPUT);
+	pinMode(muxOutputPin, INPUT);
   
-	digitalWrite(4, LOW);
-	digitalWrite(5, LOW);
-	digitalWrite(6, HIGH);
-	state = digitalRead(3);
-	nodeID += state * 8;
-	digitalWrite(4, HIGH);
-	digitalWrite(5, LOW);
-	digitalWrite(6, HIGH);
-	state = digitalRead(3);
-	nodeID += state * 4;
-	digitalWrite(4, LOW);
-	digitalWrite(5, HIGH);
-	digitalWrite(6, HIGH);
-	state = digitalRead(3);
-	nodeID += state * 2;
-	digitalWrite(4, HIGH);
-	digitalWrite(5, HIGH);
-	digitalWrite(6, HIGH);
-	state = digitalRead(3);
-	nodeID += state;
+	digitalWrite(muxCPin, LOW); digitalWrite(muxBPin, LOW); digitalWrite(muxAPin, LOW);
+	nodeID |= digitalRead(muxOutputPin) << 0;
 
-	out.nodeid = nodeID;
- 
+	digitalWrite(muxCPin, LOW); digitalWrite(muxBPin, LOW); digitalWrite(muxAPin, HIGH);
+	nodeID |= digitalRead(muxOutputPin) << 1;
+
+	digitalWrite(muxCPin, LOW); digitalWrite(muxBPin, HIGH); digitalWrite(muxAPin, LOW);
+	nodeID |= digitalRead(muxOutputPin) << 2;
+
+	digitalWrite(muxCPin, LOW); digitalWrite(muxBPin, HIGH); digitalWrite(muxAPin, HIGH);
+	nodeID |= digitalRead(muxOutputPin) << 3;
+
+	digitalWrite(muxCPin, HIGH); digitalWrite(muxBPin, LOW); digitalWrite(muxAPin, LOW);
+	nodeID |= digitalRead(muxOutputPin) << 4;
+
+	digitalWrite(muxCPin, HIGH); digitalWrite(muxBPin, LOW); digitalWrite(muxAPin, HIGH);
+	nodeID |= digitalRead(muxOutputPin) << 5;
+
+	digitalWrite(muxCPin, HIGH); digitalWrite(muxBPin, HIGH); digitalWrite(muxAPin, LOW);
+	nodeID |= digitalRead(muxOutputPin) << 6;
+
+	digitalWrite(muxCPin, HIGH); digitalWrite(muxBPin, HIGH); digitalWrite(muxAPin, HIGH);
+	nodeID |= digitalRead(muxOutputPin) << 7;
+
+	out.nodeid = nodeID;	
+
+	pinMode(muxOutputPin, OUTPUT);
+
+	digitalWrite(muxAPin, LOW);
+	digitalWrite(muxBPin, LOW);
+	digitalWrite(muxCPin, LOW);
+	digitalWrite(muxOutputPin, LOW);
+
 	return nodeID;
 }
 
 //this method returns the current farenheight temperature from the sensor;
-void getCurrentTemperature()
+void updateCurrentTemperature()
 {
 	out.currentTemperature = (int) tempSensor.readTemperature(true);
 }
 
-void getCurrentPressure()
+void updateCurrentPressure()
 {
-	char status;
-	double T, P, p0, a;
+	out.currentPressure = analogRead(A4);
+}
 
-	status = pressure.startTemperature();
-	if (status != 0)
-	{
-		delay(status);
-		status = pressure.getTemperature(T);
-		if (status != 0)
-		{
-			status = pressure.startPressure(3);
-			if (status != 0)
-			{
-				delay(status);
-				status = pressure.getPressure(P, T);
-				if (status != 0)
-				{
-					out.currentPressure = P;
-				}
-			}
-		}
-	}
+void setFanSpeed(int val)
+{
+	analogWrite(fanControlPin, val);
+}
+
+void updateMotionDetected()
+{
+	out.isMotionDetected = digitalRead(motionSensorPin);
 }
 
 void setup() 
 {
 	delay(2000);
-	myservo.attach(SERVO_PIN_A);
-	mesh.setNodeID(getNodeID());
+
+	pinMode(motionSensorPin, INPUT);
+	pinMode(fanControlPin, OUTPUT);
+
+	myservo.attach(servoPin);
+
+	mesh.setNodeID(updateNodeID());
+
 	mesh.begin();
+
 	tempSensor.begin();
-	pressure.begin();
+
+  myservo.write(90);
+  out.currentServoPosition = 90;
 }
 
 void loop()
 {
-	getCurrentTemperature();
-	getCurrentPressure();	
+  delay(2000);
+  
+	if (out.currentPressure == 0 || out.currentFanSpeed == 0)
+		digitalWrite(fanPowerPin, LOW);
+	else
+		digitalWrite(fanPowerPin, HIGH);
 
+	updateCurrentTemperature();
+	updateCurrentPressure();	
+	updateMotionDetected();
 	mesh.update();
 
 	mesh.write(&out, 'M', sizeof(out));
+
 	while (network.available())
 	{
 		RF24NetworkHeader header;
@@ -137,24 +159,33 @@ void loop()
 
 		switch (header.type)
 		{
+			//if there is incoming data
 			case 'N':
         		incomingData in;
+				// get it
 				network.read(header, &in, sizeof(in));
+			
+				//if the servo position needs to change
 				if (in.servoPosition != out.currentServoPosition)
 				{
-				  out.currentServoPosition = in.servoPosition;
-				  myservo.write(out.currentServoPosition);
+					// update it in the outgoing data
+				  	out.currentServoPosition = in.servoPosition;
+					//and write it to the servo
+				  	myservo.write(out.currentServoPosition);
 				}
+
+				// if the fan speed needs to change
 				if (in.fanSpeed != out.currentFanSpeed)
 				{
-				  out.currentFanSpeed = in.fanSpeed;
+					// update it in the outgoing data
+				  	out.currentFanSpeed = in.fanSpeed;
+
+					// and write it to the fan
+					setFanSpeed(in.fanSpeed);
 				}
 				break;
 			default:
-				payload_t payload;
-				network.read(header, &payload, sizeof(payload));
 				break;
 		}
 	}
-	delay(2000);
 }
